7b23dcbe2268fe62b5cb651db4625bf65947f7ec
EMJJones <100976581+EMJJones@users.noreply.github.com>
2022-04-08 16:25:48 -0700

    Add files via upload

---
+++ Senior Project/Senior Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Senior Project
+//
+//  Created by EJones on 4/1/22.
+//  Copyright © 2022 EJones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
